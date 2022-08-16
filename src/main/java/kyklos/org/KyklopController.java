6977f2e46dc830e9bf868188c76c81cd6a17b008
GottNereus <jan.schefer@noseryoung.ch>
2022-08-16 09:15:34 +0200

    all endpoints finished

--- src/main/java/kyklos/org/KyklopController.java
+++ src/main/java/kyklos/org/KyklopController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.InstanceNotFoundException;
 import javax.validation.Valid;
 
 
@@ -15,20 +16,38 @@ public class KyklopController {
 
 
     private KyklopService kService;
+
     @Autowired
     public KyklopController(KyklopService kService) {this.kService = kService;}
 
 
+    // Get
 
-        @GetMapping("/")
-    public String test() {
-            return "BEER IS THE TRUTH";
+    @GetMapping("/{kyklopId}")
+    public ResponseEntity<Kyklop> getKyklop(@Valid @PathVariable("kyklopId") Integer kyklopId) throws InstanceNotFoundException {
+        return ResponseEntity.ok().body( kService.getKyklop(kyklopId));
+    }
 
-        }
+    // Put
+    @PutMapping("/")
+    public ResponseEntity<Kyklop> putKyklop(@Valid @RequestBody Kyklop kyklop){
+        kService.putKyklop(kyklop);
+        return ResponseEntity.status(HttpStatus.CREATED).body(kyklop);
+    }
 
-    @PostMapping(value = "/")
+    // Post
+
+    @PostMapping("/")
         public ResponseEntity<Kyklop> postKyklop(@Valid @RequestBody Kyklop newKyklop){
             kService.postKyklop(newKyklop);
             return ResponseEntity.status(HttpStatus.CREATED).body(newKyklop);
+    }
+
+    // Delete
+
+    @DeleteMapping("/{kyklopId}")
+    public ResponseEntity<String> deleteKyklop(@Valid @PathVariable("kyklopId") Integer kyklopId) throws InstanceNotFoundException {
+        kService.deleteKyklop(kyklopId);
+        return ResponseEntity.ok().body("User with ID "+ kyklopId +" deleted successfully");
     }
 }
--- src/main/java/kyklos/org/KyklopService.java
+++ src/main/java/kyklos/org/KyklopService.java
@@ -3,6 +3,8 @@ package kyklos.org;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.management.InstanceNotFoundException;
 import javax.transaction.Transactional;
 
 @Service
@@ -17,11 +19,25 @@ public class KyklopService {
         this.kRepo = kRepo;
     }
 
-        // endpoint 1 get
+    // endpoint 1 get
 
-        // endpoint 2 put
+    public Kyklop getKyklop(Integer id) throws InstanceNotFoundException{
+        return (kRepo.findById(id).orElseThrow(() -> new InstanceNotFoundException("Kyklop with id"+id+"doesn't exist")));
 
-        // endpoint 3 post
+    }
+
+    // endpoint 2 put
+
+    public void putKyklop(Kyklop kyklop) {
+        if (!(kRepo.existsById(kyklop.getId()))) {
+            log.info("Kyklop was added");
+            kRepo.save(kyklop);
+        } else {
+            log.info("Kyklop was updated");
+            kRepo.save(kyklop);       }
+    }
+
+    // endpoint 3 post
 
     public void postKyklop(Kyklop kyklop) {
         if (!(kRepo.existsById(kyklop.getId()))) {
@@ -30,8 +46,13 @@ public class KyklopService {
         } else {
             log.warn("User already exists");
         }
-
+    }
         // endpoint 4 delete
 
+        public void deleteKyklop(Integer id) throws InstanceNotFoundException {
+            kRepo.findById(id).orElseThrow(() -> new InstanceNotFoundException("Kyklop with id"+id+"doesn't exist"));
+            log.info("Kyklop was deleted");
+            kRepo.deleteById(id);
+        }
     }
-}
+
